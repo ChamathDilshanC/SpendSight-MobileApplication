7e5133df2cfc2c67ce0336af56c66fd1063923c3
ChamathDilshanC <dilshancolonne123@gmail.com>
2025-09-14 13:51:09 +0530

    Add Get Started layout and screen components
    Introduces GetStartedLayout for wrapping children and GetStartedScreen with basic UI. These components set up the structure for the 'Get Started' section of the app.

---
+++ app/(getStarted)/getStartedScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const GetStartedScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-2xl font-bold text-[#0077CC]">Get Started</Text>
+    </View>
+  );
+};
+
+export default GetStartedScreen;
