f197d2344abe2f8f15d537e8163ae1ed0097197d
ChamathDilshanC <dilshancolonne123@gmail.com>
2025-09-22 13:30:38 +0530

    Remove extra whitespace in navigationManager.ts
    Cleaned up unnecessary whitespace to improve code readability.

--- utils/navigationManager.ts
+++ utils/navigationManager.ts
@@ -114,7 +114,7 @@ export class NavigationManager {
     this.safeNavigate("/(auth)/login", "replace");
   }
 
-  
+
   static navigateToAuth(): void {
     this.safeNavigate("/(auth)/login", "replace");
   }
